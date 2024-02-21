e7af28ffbd2a87607cd1282e26d5faf078f8c1d8
Riyal Riyan <riyel1422@gmail.com>
2024-02-21 09:13:06 +0600

    added two texts and two text-fields

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.bmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.bmicalculator.ui.theme.BMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BMICalculator()
+                    BMICalculator(12,40)
                 }
             }
         }
@@ -34,9 +39,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BMICalculator(){
+fun BMICalculator(height:Int, weight: Int){
     Surface(modifier = Modifier.fillMaxWidth()) {
-        Text(text = "BMI", textAlign = TextAlign.Center, fontSize = 30.sp, fontWeight = FontWeight.Bold)
-        Text(text = "Calculator", textAlign = TextAlign.Center, fontSize = 15.sp)
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "BMI", fontSize = 100.sp, fontWeight = FontWeight.Bold, color = Color.Gray)
+            Text(text = "Calculator", fontSize = 50.sp)
+            TextField(value = height, onValueChange = {height.value = it})
+            TextField(value = weight, onValueChange = {weight.value = it})
+        }
     }
 }

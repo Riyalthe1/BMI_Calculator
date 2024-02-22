054803a0293967617f14a00edc5c3da22c30f8b0
Riyal Riyan <riyel1422@gmail.com>
2024-02-22 18:55:23 +0600

    textfield dekhs

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -4,20 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bmicalculator.ui.theme.BMICalculatorTheme
 
@@ -31,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BMICalculator(12,40)
+                    BMICalculator()
                 }
             }
         }
@@ -39,13 +46,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BMICalculator(height:Int, weight: Int){
+fun BMICalculator() {
+    var height = remember {
+        mutableStateOf("")
+    }
+    var weight = remember {
+        mutableStateOf("")
+    }
+
+    var bmi = remember {
+        mutableStateOf("")
+    }
     Surface(modifier = Modifier.fillMaxWidth()) {
-        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(text = "BMI", fontSize = 100.sp, fontWeight = FontWeight.Bold, color = Color.Gray)
             Text(text = "Calculator", fontSize = 50.sp)
-            TextField(value = height, onValueChange = {height.value = it})
-            TextField(value = weight, onValueChange = {weight.value = it})
+            Spacer(modifier = Modifier.height(30.dp))
+            OutlinedTextField(value = height.value, onValueChange = {height.value = it}, placeholder = "Enter your Height")
+            Spacer(modifier = Modifier.height(30.dp))
+            TextField(value = weight.value, onValueChange = { weight.value = it }, label = "Neter ")
         }
     }
 }
